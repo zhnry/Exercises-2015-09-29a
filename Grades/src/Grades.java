7011291410b17329bb6500a9fbde4afaf90f2b50
Phil O'Connell <ist242spring2015@gmail.com>
2015-09-28 23:13:07 -0400

    First step

--- Grades/src/Grades.java
+++ Grades/src/Grades.java
@@ -1,16 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Phil O'Connell <pxo4@psu.edu>
+import java.util.Scanner;
+
+/*
+ * Repeatedly prompt the user to enter a grade.
+ * 
+ * The grade must be between 0 and 100; or it can be equal to -1.
+ * Entering a -1 means the user has finished entering grades.
+ * 
+ * Output to the user
+ * - How many grades the user entered
+ * - The average of the grades
+ * - Error messages when appropriate
+ * 
+ * Sample interaction:
+ * 
+ * Enter a grade: 70
+ * Enter a grade: 75
+ * Enter a grade: 101
+ * Error. Grade must be between 0 and 100; or -1 to finish
+ * Enter a grade: -1
+ * 
+ * You entered 2 grades. The average is 72.5
  */
 public class Grades {
+
   public static void main(String[] args) {
-    System.out.println("Hello, world");
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("Enter a grade: ");
+    int grade = keyboard.nextInt();
+    System.out.println("You entered: " + grade);
+
   }
- 
+
 }
