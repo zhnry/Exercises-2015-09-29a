33cedca46c91094c0022dfe62b6e81316bb81831
Phil O'Connell <ist242spring2015@gmail.com>
2015-09-28 23:46:45 -0400

    Added Enumerates

---
+++ Grades/src/Enumerates.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Phil O'Connell <pxo4@psu.edu>
+ */
+public class Enumerates {
+
+  private class Person {
+
+    String role;
+    String name;
+
+    public Person(String name, String role) {
+      this.name = name;
+      this.role = role;
+    }
+
+    @Override
+    public String toString() {
+      return this.name + " (" + this.role + ')';
+    }
+  }
+
+  public static void main(String[] args) {
+    Enumerates enumerates = new Enumerates();
+    enumerates.start();
+  }
+
+  public void start() {
+    Person instructor = new Person("Phil", "teacher");
+    Person student = new Person("Joe", "manager");
+    System.out.println(instructor.toString());
+    System.out.println(student.toString());
+  }
+
+}
