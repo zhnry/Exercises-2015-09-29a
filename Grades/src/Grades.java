d04b1404e4c9665a50e259c43b3bd2f79a7a8eb8
hzhen <hzhen@hzhen.ad.corp.appnexus.com>
2015-09-29 08:46:12 -0400

    grade calculator

--- Grades/src/Grades.java
+++ Grades/src/Grades.java
@@ -24,14 +24,38 @@ import java.util.Scanner;
  */
 public class Grades {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
+        //Total of all grades
+        int total = 0;
+        int count = 0;
 
-    System.out.print("Enter a grade: ");
-    int grade = keyboard.nextInt();
-    System.out.println("You entered: " + grade);
+        for (;;) {
+            System.out.print("Enter a grade: ");
+            int grade;
+            try {
+            grade = keyboard.nextInt();
+            } catch (Exception e) {
+                System.out.println("Not an integer");
+                continue;
+            }
+            // Check if grade is in range
+            if (grade >= 0 && grade <= 100) {
+                total += grade;
+                count++;
+            } // If not in range then what will happen
+            else if (grade == -1) {
+                System.out.println("Value not equal to one");
+                break;
+            }
+        }
 
-  }
-
+        System.out.println("Total is: " + total);
+        System.out.println("Count is " + count);
+        if (count == 0) {
+            System.out.println("No grades Entered");
+        }
+        System.out.println("Average is: " + (float) total / count);
+    }
 }
